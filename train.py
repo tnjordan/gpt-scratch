0cd4e15a70a32b0e5d16db9080e59458b6e1cb51
todd.jordan <todd.jordan@paccar.com>
2025-08-28 21:14:44 -0700

    completed bigram @ 1:37

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # %%
 print("CUDA available:", torch.cuda.is_available())
